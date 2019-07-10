bc416b79cf536d4410b3b5ee558424f1a4b3db3d
Windy Lie <52243986+windylie@users.noreply.github.com>
2019-07-10 10:56:09 +1000

    implement post request to create new phone no

--- TelecomOperatorApi/Controllers/CustomerController.cs
+++ TelecomOperatorApi/Controllers/CustomerController.cs
@@ -3,18 +3,41 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TelecomOperatorApi.Models;
 
 namespace TelecomOperatorApi.Controllers
 {
     [Route("api/customers")]
     public class CustomerController : Controller
     {
-        [HttpGet("{customerId}/phones")]
+        [HttpGet("{customerId}/phones", Name = "GetCustomerPhones")]
         public IActionResult GetAllCustomerPhoneNumbers(int customerId)
         {
             var phonesToReturn = PhoneDataStore.Current.AllPhoneNumbers.Where(c => c.CustomerId == customerId);
 
             return Ok(phonesToReturn);
+        }
+
+        [HttpPost("{customerId}/phones")]
+        public IActionResult CreatePhoneNumber(int customerId, [FromBody] CreatePhoneRequestDto newPhoneNo)
+        {
+            if (newPhoneNo == null)
+            {
+                return BadRequest();
+            }
+
+            var maxPhoneId = PhoneDataStore.Current.AllPhoneNumbers.Max(p => p.Id);
+            var newPhoneData = new PhoneDto()
+            {
+                Id = ++maxPhoneId,
+                CustomerId = customerId,
+                PhoneNo = newPhoneNo.PhoneNo,
+                Activated = false
+            };
+
+            PhoneDataStore.Current.AllPhoneNumbers.Add(newPhoneData);
+
+            return CreatedAtRoute("GetCustomerPhones", new { customerId = customerId, newPhoneNo = newPhoneData.PhoneNo }, newPhoneData);
         }
     }
 }
